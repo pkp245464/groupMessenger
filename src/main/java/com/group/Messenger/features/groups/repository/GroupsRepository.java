b08afff0cfd4ad39357ea5b218dc7cc5217ba0a4
Pankaj Kumar <pankajkumar245464@gmail.com>
2024-12-28 20:51:11 +0530

    minor changes

--- src/main/java/com/group/Messenger/features/groups/repository/GroupsRepository.java
+++ src/main/java/com/group/Messenger/features/groups/repository/GroupsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface GroupsRepository extends JpaRepository<Groups,String> {
+public interface GroupsRepository extends JpaRepository<Groups,Long> {
     Optional<Groups> findByGroupName(String groupName);
     Optional<Groups> findByGroupIdAndIsDeletedFalse(Long groupId);
     Optional<Groups> findByGroupNameAndIsDeletedFalse(String groupName);
